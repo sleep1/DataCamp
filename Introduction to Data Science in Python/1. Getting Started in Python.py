5b94adbecf56f19325eb99f5793782634ddef685
sleep1 <omar.om.castelo@gmail.com>
2020-10-04 22:13:23 -0400

    Create 1. Getting Started in Python.py

---
+++ Introduction to Data Science in Python/1. Getting Started in Python.py
@@ -0,0 +1,64 @@
+Fix the import of numpy to run without errors.
+
+# Fix the import of numpy to run without errors
+import numpy as np
+
+Define a variable called bayes_age and set it equal to 4.0.
+Display the variable bayes_age.
+
+# Fill in Bayes' age (4.0)
+bayes_age = 4.0
+
+# Display the variable bayes_age
+print(bayes_age)
+
+Define a variable called favorite_toy whose value is "Mr. Squeaky".
+Define a variable called owner whose value is 'DataCamp'.
+Show the values assigned to these variables.
+
+# Bayes' favorite toy
+favorite_toy = "Mr. Squeaky"
+
+# Bayes' owner
+owner = 'DataCamp'
+
+# Display variables
+print(favorite_toy)
+print(owner)
+
+Correct the mistakes in the code so that it runs without producing syntax errors.
+
+# One or more of the following lines contains an error
+# Correct it so that it runs without producing syntax errors
+birthday = "2017-07-14"
+case_id = 'DATACAMP!123-456?'
+
+Use pd.read_csv to load data from a CSV file called ransom.csv.
+This file represents the frequency of each letter in the ransom note for Bayes.
+
+# Import pandas
+import pandas as pd
+
+# Load the 'ransom.csv' into a DataFrame
+r = pd.read_csv('ransom.csv')
+
+# Display DataFrame
+print(r)
+
+Correct the code so that it runs without syntax errors
+# One or more of the following lines contains an error
+# Correct it so that it runs without producing syntax errors
+
+# Plot a graph
+plt.plot(x_values, y_values)
+
+# Display the graph
+plt.show()
+
+Create a variable called plate that represents the observed license plate: the first three letters were FRQ, but the witness couldn't see the final 4 letters.
+Use asterisks (*) to represent missing letters.
+
+# Define plate to represent a plate beginning with FRQ
+# Use * to represent the missing four letters
+plate = "FRQ****"
+
